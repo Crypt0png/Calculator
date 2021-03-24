097c5285592bc6ac15a6d77dfb6f50e6c5a55e9f
Maksim <81237381+Crypt0png@users.noreply.github.com>
2021-03-24 15:52:02 +0300

    Update and rename source code(english version) to source code(english version).py

---
+++ My code/source code(english version).py
@@ -0,0 +1,40 @@
+
+a = input('Select the calculator type(addition, multiplication, or subtraction): ')
+b = int(input('Enter the number of numbers that you need to perform operations with(up to 3): '))
+
+if a == 'multiplication':
+  if b == 3:
+    number1 = int(input('Enter the first number: '))
+    number2 = int(input('Enter the second number: '))
+    number3 = int(input('Enter the third number: '))
+    print(number1 * number2 * number3)
+  elif b == 2:
+    number1 = int(input('Enter the first number: '))
+    number2 = int(input('Enter the second number: '))
+    print(number1 * number2)
+  else:
+    print('NO')
+elif a == 'addition':
+  if b == 3:
+    number1 = int(input('Enter the first number: '))
+    number2 = int(input('Enter the second number: '))
+    number3 = int(input('Enter the third number: '))
+    print(number1 + number2 + number3)
+  elif b == 2:
+    number1 = int(input('Enter the first number: '))
+    number2 = int(input('Enter the second number: '))
+    print(number1 + number2)
+  else:
+    prin('NO')
+elif a == 'subtraction':
+  if b == 3:
+    number1 = int(input('Enter the first number: '))
+    number2 = int(input('Enter the second number: '))
+    number3 = int(input('Enter the third number: '))
+    print(number1 - number2 - number3)
+  elif b == 2:
+    number1 = int(input('Enter the first number: '))
+    number2 = int(input('Enter the second number: '))
+    print(number1 - number2)
+  else:
+    print('NO')
